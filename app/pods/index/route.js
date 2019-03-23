618b4126edfdb0d46c5b9e1510f507028d647c7b
Zachary Nicoll <github@zdn.me>
2019-03-23 18:17:05 -0500

    Add code pens to home/css page

---
+++ app/pods/index/route.js
@@ -0,0 +1,25 @@
+import Route from '@ember/routing/route'
+import Parser from 'npm:rss-parser'
+
+export default Route.extend({
+  model() {
+    return new Promise(function(resolve, reject) {
+      let parser = new Parser()
+      let url = 'https://codepen.io/Turtleguyy/public/feed'
+      let feed = parser.parseURL(url).then((json) => {
+        resolve(json)
+      }, (error) => {
+        reject(error)
+      })
+    })
+  },
+
+  setupController(controller, model) {
+    let pens = []
+    for (let item of model.items.slice(0, 9)) {
+      pens.push(/.*pen\/(.*)/.exec(item.link)[1])
+    }
+
+    controller.set('pens', pens)
+  },
+})
