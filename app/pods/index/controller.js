dd7f7c52d4f08b57a6da4161707f9b7d4a46e9ed
Zachary Nicoll <github@zdn.me>
2019-04-01 17:08:19 -0500

    Add default info for the defaults.

--- app/pods/index/controller.js
+++ app/pods/index/controller.js
@@ -2,6 +2,16 @@ import Controller from '@ember/controller'
 
 export default Controller.extend({
 
-  pens: ['vPRboo', 'NJwwrO', 'moBEVL', 'WPPQpG', 'PVyYOP', 'LqBvqg', 'bzKJNb', 'moJZrg'],
+  pens: [
+    { code: 'vPRboo', title: 'CSS Challenge - Day 69' },
+    { code: 'NJwwrO', title: 'CSS Challenge - Day 66' },
+    { code: 'moBEVL', title: 'CSS Challenge - Day 65' },
+    { code: 'moJZrg', title: 'CSS Challenge - Day 53' },
+    { code: 'WPPQpG', title: 'CSS Challenge - Day 44' },
+    { code: 'PVyYOP', title: 'CSS Challenge - Day 40' },
+    { code: 'LqBvqg', title: 'CSS Challenge - Day 39' },
+    { code: 'bzKJNb', title: 'CSS Challenge - Day 38' },
+    { code: 'dXpyme', title: 'Wheel o\' Fortune' },
+  ],
 
 })
