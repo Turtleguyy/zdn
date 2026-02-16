30ea19cb93416a583e509bdea6f6545a5098c1f4
Zachary Nicoll <zach@agencykpi.com>
2026-02-16 17:32:42 -0600

    Fixes css route import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { ThemeEffect } from "./ThemeEffect";
 import { Layout } from "./layout/Layout";
 import { Home } from "./routes/Home";
-import { Css } from "./routes/Css";
+import { CSS } from "./routes/CSS";
 import { Projects } from "./routes/Projects";
 import { CaseStudies } from "./routes/CaseStudies";
 
@@ -10,11 +10,11 @@ export default function App() {
   return (
     <>
       <ThemeEffect />
-      <div className="p-5 md:p-6 flex flex-row max-[700px]:flex-col text-copy transition-colors duration-200 min-h-screen">
+      <div className="text-copy flex min-h-screen flex-row p-5 transition-colors duration-200 max-[700px]:flex-col md:p-6">
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/css" element={<Css />} />
+            <Route path="/css" element={<CSS />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/case-studies" element={<CaseStudies />} />
           </Routes>
