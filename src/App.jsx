32948439d6fde69d6941e618f76c845006e42bf0
Zachary Nicoll <zach@agencykpi.com>
2026-02-16 18:13:10 -0600

    Whoops

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { ThemeEffect } from "./ThemeEffect";
 import Layout from "./layout/Layout";
 import CaseStudies from "./routes/CaseStudies";
-import Css from "./routes/CSS";
+import CSS from "./routes/CSS";
 import Home from "./routes/Home";
 import Projects from "./routes/Projects";
 
