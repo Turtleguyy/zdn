4849e10d801a20b2255c7fec5ad620b2afb3c9c2
Zachary Nicoll <github@zdn.me>
2026-03-08 22:21:35 -0500

    Update Home.jsx
    Apparently I was too "informal" or something

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -16,13 +16,13 @@ export default function Home() {
         <a href="https://www.agencykpi.com/" target="_blank" rel="noreferrer">
           AgencyKPI
         </a>{" "}
-        working on all sorts of cool things. Right now, I&rsquo;m focused on
+        working on things like complex data visualizations, component libraries,
+        internal tooling, and everything in between. Right now, I&rsquo;m focused on
         finding the next great team to build with. I love what I do, but I also
-        love life and everything that goes along with it. Yes, I&rsquo;m a
-        software developer, but more importantly, I&rsquo;m a father, a husband,
-        a diver, and a gamer. Perhaps even more paramount to the core of who I
-        am, I&rsquo;m also a strong believer in the oxford comma. That, and the
-        fact that it&rsquo;s{" "}
+        love life and everything that goes along with it. I&rsquo;m a software
+        engineer by day, and a father, husband, diver, gamer, and woodworker by
+        night. Perhaps even more paramount to the core of who I am, I&rsquo;m also
+        a strong believer in the oxford comma. That, and the fact that it&rsquo;s{" "}
         <span title="yes, even though my own name contradicts this">
           sync and not synch
         </span>{" "}
@@ -32,20 +32,12 @@ export default function Home() {
       <h2>My Sweet Skills</h2>
 
       <p>
-        Most days I&apos;m a front-end/product engineer focused on React
-        applications and complex UI workflows. I still care deeply about the
-        fundamentals: semantic HTML, thoughtful CSS, and JavaScript that is
-        readable six months later. CSS is still my jam, and yes, I still prefer
-        writing it by hand when it makes sense.
-      </p>
-
-      <p>
-        My roots are in Haml, Sass, CoffeeScript, and Ruby on Rails. These
-        days, most of my production work is React and modern JavaScript, but
-        those earlier stacks taught me habits I still rely on: clear structure,
-        sensible abstractions, and respect for maintainability. Rails still has
-        a special place in my heart, and yes, like everyone else who&apos;s
-        honest with themselves, I still miss CoffeeScript.
+        My roots are in Haml, Sass, CoffeeScript, and Ruby on Rails. These days,
+        however, most of my time is spent in React, TypeScript, and my good
+        friend, CSS. I use those tools to write semantic, accessible,
+        thoughtful code that still makes sense six months later. Rails will
+        always have a special place in my heart and I'd welcome the chance to
+        pick it back up.
       </p>
 
       <p>
@@ -54,13 +46,12 @@ export default function Home() {
         <span title="alphabetical, because I'm not a monster">
           no particular
         </span>{" "}
-        order: Bash, C#, CSS, Ember.js, Erb, Git, GraphQL, Haml, HTML,
-        Illustrator, JavaScript,{" "}
-        <span title="talk about glory days, huh?">jQuery</span>, Objective-C,
-        Photoshop, React, Regex, Ruby on Rails, Sass, SQL, and Swift. I've also
-        used things like ActionScript, ColdFusion, Flex, and PHP in the past,
-        but I'm quite certain I've forgotten almost everything I ever knew about
-        them.
+        order: AWS, Bash, Bitbucket, C#, CSS, Ember.js, Erb, Figma, Git,
+        Github, GraphQL, Haml, HTML, Illustrator, JavaScript, Jenkins,{" "}
+        <span title="talk about glory days, huh?">jQuery</span>, LaunchDarkly,
+        Objective-C, Photoshop, Postgres, React, Regex, Ruby on Rails, Sass,
+        Swift, and TypeScript. I've also used things like ActionScript,
+        ColdFusion, Flex, and PHP in the past.
       </p>
 
       <h2>How'd I get those sweet skills?</h2>
@@ -74,28 +65,30 @@ export default function Home() {
         >
           Full Sail University
         </a>{" "}
-        in 2011 with a BS in Web Design & Development. They gave me a pretty
-        solid foundation in full-stack web development, with the added{" "}
+        in 2011 with a BS in Web Design & Development. They gave me a solid
+        foundation in full-stack web development, with the added{" "}
         <span title="ignorance is definitely bliss">curse</span> of
-        understanding UX principles. Right before I graduated, I landed a job in
-        Colorado as a front-end developer and off I went into the world of Web.
+        understanding UX principles. Right before I graduated, I landed a job
+        at a start-up in Colorado as a frontend developer and off I went into
+        the world of Web.
       </p>
 
       <p>
-        I only had that first job for a few short months, because{" "}
+        Half a year later,{" "}
         <a
-          href="https://twitter.com/alyssanicoll"
+          href="https://alyssanicoll.com/"
           target="_blank"
           rel="noreferrer"
         >
           my wife
         </a>{" "}
-        decided she wanted to do what I did. So, I got a new job in Orlando as
-        a web developer, and back to Florida (and Full Sail) we went. I worked
-        in an office for about five years, then switched to remote full-time in
-        2017. I&apos;ve had the opportunity to try my hand at a few different
-        technologies over the course of many different projects throughout my
-        career. Be sure to check out the{" "}
+        decided that she, too, wanted to learn the wonders of CSS. So, I got a new
+        job as a full-stack developer at a start-up in Orlando and back to
+        Florida (and Full Sail) we went. I worked in an office for about five
+        years, then switched to remote full-time in 2017. I&apos;ve had the
+        opportunity to try my hand at a few different technologies over the
+        course of many different projects throughout my career. Be sure to
+        check out the{" "}
         <Link to="/experience">experience</Link> page for more details.
       </p>
 
@@ -103,7 +96,7 @@ export default function Home() {
 
       <p>
         Lastly, and contrary to popular belief, no the "Good Morning!" heading
-        won't change based on the time of day. Sure, I could have done that, but
+        won't change based on the time of day. Sure, I could do that, but
         who's to say it's your morning, afternoon, or evening? It's {year}{" "}
         <span title="he said, in full awareness of the irony (and because he's hilarious)">
           guys
